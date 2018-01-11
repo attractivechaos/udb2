ac868d16d26fde3c2ee6661c483c6eed7e60d483
Heng Li <lh3@me.com>
2018-01-11 00:20:23 -0500

    changed to 32-bit keys
    it is intricate to use 64-bit keys with uthash

--- khash/test.c
+++ khash/test.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 
-static inline uint64_t hash64(uint64_t key)
+static inline uint32_t hash32(uint32_t key)
 {
-	key = ~key + (key << 21);
-	key = key ^ key >> 24;
-	key = (key + (key << 3)) + (key << 8);
-	key = key ^ key >> 14;
-	key = (key + (key << 2)) + (key << 4);
-	key = key ^ key >> 28;
-	key = key + (key << 31);
-	return key;
+    key += ~(key << 15);
+    key ^=  (key >> 10);
+    key +=  (key << 3);
+    key ^=  (key >> 6);
+    key += ~(key << 11);
+    key ^=  (key >> 16);
+    return key;
 }
 
 static double cputime(void)
@@ -28,29 +28,29 @@ double time_rng(uint64_t n, uint64_t x0)
 	double t;
 	t = cputime();
 	for (i = 0, x = x0; i < n; ++i)
-		x = hash64(x);
+		x = hash32(x);
 	return cputime() - t;
 }
 
 #include "khash.h"
-KHASH_INIT(64, uint64_t, uint64_t, 1, hash64, kh_int64_hash_equal)
+KHASH_INIT(32, uint32_t, uint32_t, 1, hash32, kh_int_hash_equal)
 
-double time_64(uint64_t n, uint64_t x0)
+double time_32(uint64_t n, uint64_t x0)
 {
 	double t;
 	uint64_t i, x;
-	khash_t(64) *h;
+	khash_t(32) *h;
 	t = cputime();
-	h = kh_init(64);
+	h = kh_init(32);
 	for (i = 0, x = x0; i < n; ++i) {
 		khint_t k;
 		int absent;
-		x = hash64(x);
-		k = kh_put(64, h, x % (n/2), &absent);
+		x = hash32(x);
+		k = kh_put(32, h, x % (n/2), &absent);
 		if (absent) kh_val(h, k) = 0;
 		++kh_val(h, k);
 	}
-	kh_destroy(64, h);
+	kh_destroy(32, h);
 	fprintf(stderr, "# unique keys: %d\n", kh_size(h));
 	return cputime() - t;
 }
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	if (argc > 1) n = atol(argv[1]);
 	t0 = time_rng(n, x0);
 	fprintf(stderr, "CPU time spent on RNG: %.3f sec\n", t0);
-	t1 = time_64(n, x0);
+	t1 = time_32(n, x0);
 	fprintf(stderr, "CPU time (n=%ld): %.3f sec\n", (long)n, t1 - t0);
 	return 0;
 }
