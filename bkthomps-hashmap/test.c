8dca7272892b743f27be35155f09f46549318acf
Heng Li <lh3@me.com>
2018-01-11 23:06:10 -0500

    finalized bkthomps-hashmap

--- bkthomps-hashmap/test.c
+++ bkthomps-hashmap/test.c
@@ -1,49 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-
-static inline uint32_t hash32(uint32_t key)
-{
-    key += ~(key << 15);
-    key ^=  (key >> 10);
-    key +=  (key << 3);
-    key ^=  (key >> 6);
-    key += ~(key << 11);
-    key ^=  (key >> 16);
-    return key;
-}
-
-static double cputime(void)
-{
-	struct rusage r;
-	getrusage(RUSAGE_SELF, &r);
-	return r.ru_utime.tv_sec + r.ru_stime.tv_sec + 1e-6 * (r.ru_utime.tv_usec + r.ru_stime.tv_usec);
-}
-
-double time_rng(uint32_t n, uint32_t x0)
-{
-	uint32_t i, x;
-	double t;
-	t = cputime();
-	for (i = 0, x = x0; i < n; ++i)
-		x = hash32(x);
-	return cputime() - t;
-}
-
-int main(int argc, char *argv[])
-{
-	double time_32(uint32_t n, uint32_t x0);
-	double t0, t1;
-	uint32_t n = 10000000, x0 = 1;
-	if (argc > 1) n = atol(argv[1]);
-	t0 = time_rng(n, x0);
-	fprintf(stderr, "CPU time spent on RNG: %.3f sec\n", t0);
-	t1 = time_32(n, x0);
-	fprintf(stderr, "CPU time (n=%d): %.3f sec\n", n, t1 - t0);
-	return 0;
-}
+#include "../common.c"
 
 // https://github.com/bkthomps/Containers
 // checked out on 2018-01-11
@@ -58,21 +13,18 @@ static int compare_int(const void *const one, const void *const two) // FIXME: t
 
 static unsigned long hash_int(const void *const key)
 {
-	uint32_t x = *(uint32_t*)key;
-    return hash32(x);
+	return hash32(*(uint32_t*)key);
 }
 
-double time_32(uint32_t n, uint32_t x0)
+void test_int(uint32_t n, uint32_t x0)
 {
-	double t;
-	uint32_t i, x;
+	uint32_t i, x, z = 0;
 	unordered_map h;
-	t = cputime();
 	h = unordered_map_init(sizeof(uint32_t), sizeof(uint32_t), hash_int, compare_int);
 	for (i = 0, x = x0; i < n; ++i) {
 		uint32_t key, v;
 		x = hash32(x);
-		key = x % (n/2);
+		key = get_key(n, x);
 		if (!unordered_map_get(&v, h, &key)) {
 			v = 1;
 			unordered_map_put(h, &key, &v);
@@ -80,8 +32,8 @@ double time_32(uint32_t n, uint32_t x0)
 			++v;
 			unordered_map_put(h, &key, &v);
 		}
+		z += v;
 	}
-	fprintf(stderr, "# unique keys: %d\n", unordered_map_size(h));
+	fprintf(stderr, "# unique keys: %d; checksum: %u\n", unordered_map_size(h), z);
 	unordered_map_destroy(h);
-	return cputime() - t;
 }
