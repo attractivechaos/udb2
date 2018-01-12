41b6b38b66efa35fc4a2f334a029d4b21bdad559
Heng Li <lh3@me.com>
2018-01-12 13:02:42 -0500

    a few non-standard gcc extensions

---
+++ _gcc_ext/test-2.cpp
@@ -0,0 +1,19 @@
+#include "../common.c"
+#include <tr1/unordered_map>
+
+struct Hash32 {
+	inline size_t operator()(const uint32_t x) const {
+		return hash32(x);
+	}
+};
+
+void test_int(uint32_t n, uint32_t x0)
+{
+	uint32_t i, x, z = 0;
+	std::tr1::unordered_map<uint32_t, uint32_t, Hash32> h;
+	for (i = 0, x = x0; i < n; ++i) {
+		x = hash32(x);
+		z += ++h[get_key(n, x)];
+	}
+	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
+}
---
+++ _gcc_ext/test.cpp
@@ -0,0 +1,19 @@
+#include "../common.c"
+#include <ext/pb_ds/assoc_container.hpp>
+
+struct Hash32 {
+	inline size_t operator()(const uint32_t x) const {
+		return hash32(x);
+	}
+};
+
+void test_int(uint32_t n, uint32_t x0)
+{
+	uint32_t i, x, z = 0;
+	__gnu_pbds::cc_hash_table<uint32_t, uint32_t, Hash32> h;
+	for (i = 0, x = x0; i < n; ++i) {
+		x = hash32(x);
+		z += ++h[get_key(n, x)];
+	}
+	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
+}
