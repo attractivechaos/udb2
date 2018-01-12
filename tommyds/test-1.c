728160dbe79de895ab06eecdc7563199f394e600
Heng Li <lh3@me.com>
2018-01-11 22:46:07 -0500

    finalize tommyDS

--- tommyds/test-1.c
+++ tommyds/test-1.c
@@ -2,7 +2,7 @@
 
 // https://github.com/amadvance/tommyds
 // checked out on 2018-01-11
-#include "tommyhashlin.h"
+#include "tommytrie.h"
 
 typedef struct {
 	uint32_t key;
@@ -10,29 +10,26 @@ typedef struct {
 	tommy_node node;
 } intcell_t;
 
-int compare(const void* arg, const void* obj)
-{
-	return *(const uint32_t*)arg != ((const intcell_t*)obj)->key;
-}
-
 void test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
-	tommy_hashlin h;
-	tommy_hashlin_init(&h);
+	tommy_allocator alloc;
+	tommy_trie h;
+	 tommy_allocator_init(&alloc, TOMMY_TRIE_BLOCK_SIZE, TOMMY_TRIE_BLOCK_SIZE);
+	tommy_trie_init(&h, &alloc);
 	for (i = 0, x = x0; i < n; ++i) {
 		uint32_t key;
 		intcell_t *p;
 		x = hash32(x);
 		key = get_key(n, x);
-		p = tommy_hashlin_search(&h, compare, &key, hash32(key));
+		p = tommy_trie_search(&h, key);
 		if (!p) {
 			p = malloc(sizeof(intcell_t));
 			p->key = key, p->cnt = 1;
-			tommy_hashlin_insert(&h, &p->node, p, hash32(key));
+			tommy_trie_insert(&h, &p->node, p, p->key);
 			++z;
 		} else z += ++p->cnt;
 	}
-	fprintf(stderr, "# unique keys: %d; checksum: %u\n", tommy_hashlin_count(&h), z);
-	tommy_hashlin_done(&h);
+	fprintf(stderr, "# unique keys: %d; checksum: %u\n", tommy_trie_count(&h), z);
+	tommy_allocator_done(&alloc);
 }
