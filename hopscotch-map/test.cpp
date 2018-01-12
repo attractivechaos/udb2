91c3a2d82101c732fc9f10156e1b35fb154aacca
Heng Li <lh3@me.com>
2018-01-12 10:13:20 -0500

    added hopscotch-map

---
+++ hopscotch-map/test.cpp
@@ -0,0 +1,16 @@
+#include "../common.c"
+
+// https://github.com/Tessil/hopscotch-map
+// cloned on 2018-01-12
+#include "hopscotch_map.h"
+
+void test_int(uint32_t n, uint32_t x0)
+{
+	uint32_t i, x, z = 0;
+	tsl::hopscotch_map<uint32_t, uint32_t> h;
+	for (i = 0, x = x0; i < n; ++i) {
+		x = hash32(x);
+		z += ++h[get_key(n, x)];
+	}
+	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
+}
