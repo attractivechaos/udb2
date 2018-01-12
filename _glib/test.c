f3b06af8c3eb888ee9afe3982506885e3ffe9dc8
Heng Li <lh3@me.com>
2018-01-12 10:44:44 -0500

    reduce memory

--- _glib/test.c
+++ _glib/test.c
@@ -5,19 +5,18 @@ void test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
 	GHashTable *h;
-	h = g_hash_table_new_full(g_int_hash, g_int_equal, 0, free);
+	h = g_hash_table_new_full(g_direct_hash, g_direct_equal, 0, free);
 	for (i = 0, x = x0; i < n; ++i) {
 		gpointer v;
 		uint32_t key;
 		x = hash32(x);
 		key = get_key(n, x);
-		v = g_hash_table_lookup(h, &key);
+		v = g_hash_table_lookup(h, GINT_TO_POINTER(key));
 		if (v == 0) {
-			gpointer p, q;
-			p = (uint32_t*)malloc(sizeof(uint32_t));
-			q = (uint32_t*)malloc(sizeof(uint32_t));
-			*(uint32_t*)p = key, *(uint32_t*)q = 1;
-			g_hash_table_insert(h, p, q);
+			uint32_t *cnt;
+			cnt = malloc(4);
+			*cnt = 1;
+			g_hash_table_insert(h, GINT_TO_POINTER(key), cnt);
 			++z;
 		} else z += ++*(uint32_t*)v;
 	}
