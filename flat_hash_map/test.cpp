a1a23318e30b96b1fc9d60fbbd5eed0a01cb3f42
Heng Li <lh3@me.com>
2018-01-12 14:01:11 -0500

    changed max load to 75%
    This gives no practical differences.

--- flat_hash_map/test.cpp
+++ flat_hash_map/test.cpp
@@ -15,7 +15,7 @@ void test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
 	ska::flat_hash_map<uint32_t, uint32_t, Hash32> h;
-	h.max_load_factor(0.9f);
+	h.max_load_factor(0.75f);
 	for (i = 0, x = x0; i < n; ++i) {
 		x = hash32(x);
 		z += ++h[get_key(n, x)];
