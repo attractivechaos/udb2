b43dadb56495db8f7a964b4bc49275fe5140b368
Attractive Chaos <attractor@live.co.uk>
2019-12-25 22:33:40 -0500

    add specialized hash table types

--- khashlpp/test.cpp
+++ khashlpp/test.cpp
@@ -1,34 +1,23 @@
 #include "../common.c"
 #include "khashl.hpp"
 
-struct aux_t {
-	uint32_t key, cnt;
-};
-
-struct aux_eq {
-	bool operator() (const aux_t &a, const aux_t &b) const {
-		return a.key == b.key;
-	};
-};
-
 struct aux_hash {
-	uint32_t operator() (const aux_t &a) const {
-		return hash32(a.key);
+	uint32_t operator() (uint32_t a) const {
+		return hash32(a);
 	};
 };
 
 void test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
-	klib::KHashL<aux_t, aux_hash, aux_eq> h;
+	klib::KHashMap<uint32_t, int, aux_hash> h;
 	for (i = 0, x = x0; i < n; ++i) {
-		aux_t a;
-		size_t k;
+		uint32_t k;
 		int absent;
 		x = hash32(x);
-		a.key = get_key(n, x), a.cnt = 0;
-		k = h.put(a, &absent);
-		z += ++h.at(k).cnt;
+		k = h.put(get_key(n, x), &absent);
+		if (absent) h.value(k) = 0;
+		z += ++h.value(k);
 	}
 	fprintf(stderr, "# unique keys: %d; checksum: %u; capacity: %d\n", h.size(), z, h.n_buckets());
 }
