4d044a147e90fe81f57bb1b6e813fcba19cd736b
Attractive Chaos <attractor@live.co.uk>
2019-12-26 14:47:49 -0500

    added bhopscotch_map

--- tsl_hopscotch_map/test-1.cpp
+++ tsl_hopscotch_map/test-1.cpp
@@ -2,7 +2,7 @@
 
 // https://github.com/Tessil/hopscotch-map
 // cloned on 2019-12-26
-#include "hopscotch_map.h"
+#include "bhopscotch_map.h"
 
 struct Hash32 {
 	inline size_t operator()(const uint32_t x) const {
@@ -13,7 +13,7 @@ struct Hash32 {
 void test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
-	tsl::hopscotch_map<uint32_t, uint32_t, Hash32> h;
+	tsl::bhopscotch_map<uint32_t, uint32_t, Hash32> h;
 	for (i = 0, x = x0; i < n; ++i) {
 		x = hash32(x);
 		z += ++h[get_key(n, x)];
