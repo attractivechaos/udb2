a2209163c5919cfc4ad819da164f131900f9a3cd
Attractive Chaos <attractor@live.co.uk>
2019-12-26 15:13:28 -0500

    added emilib

---
+++ emilib/test.cpp
@@ -0,0 +1,23 @@
+#include "../common.c"
+#include <algorithm> // needed by hash_map.hpp
+
+// https://github.com/emilk/emilib
+// cloned on 2019-12-26
+#include "hash_map.hpp"
+
+struct Hash32 {
+	inline size_t operator()(const uint32_t x) const {
+		return hash_fn(x);
+	}
+};
+
+void test_int(uint32_t n, uint32_t x0)
+{
+	uint32_t i, x, z = 0;
+	emilib::HashMap<uint32_t, uint32_t, Hash32> h;
+	for (i = 0, x = x0; i < n; ++i) {
+		x = hash32(x);
+		z += ++h[get_key(n, x)];
+	}
+	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
+}
