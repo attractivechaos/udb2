29def17d01ebbad1b6f79e5db8c96b7858b5d04d
Attractive Chaos <attractor@live.co.uk>
2019-12-28 10:04:18 -0500

    updated with Abseil results

--- _absl/test.cpp
+++ _absl/test.cpp
@@ -13,7 +13,12 @@ uint32_t test_int(uint32_t n, uint32_t x0)
 	absl::flat_hash_map<uint32_t, uint32_t, Hash32> h;
 	for (i = 0, x = x0; i < n; ++i) {
 		x = hash32(x);
+#ifndef UDB2_TEST_DEL
 		z += ++h[get_key(n, x)];
+#else
+		auto p = h.insert(std::pair<uint32_t, uint32_t>(get_key(n, x), i));
+		if (p.second == false) h.erase(p.first);
+#endif
 	}
 	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
 	return h.size();
