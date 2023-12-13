c398a18a816bd4d872ee719ad0492e61c226950e
Attractive Chaos <attractor@live.co.uk>
2023-12-12 23:46:55 -0500

    added boost::unordered_flat_map

--- _boost/test.cpp
+++ _boost/test.cpp
@@ -1,22 +1,20 @@
 #include "../common.c"
+#include <functional>
 
-// https://github.com/skarupke/flat_hash_map
-// cloned on 2018-09-29
-// Update on 2023-12-12: the last commit is 2018-07-15, so this should be the latest
-#include "bytell_hash_map.hpp"
+// https://github.com/martinus/robin-hood-hashing
+// cloned on 2023-12-12
+#include <boost/unordered/unordered_flat_map.hpp>
 
 struct Hash32 {
 	inline size_t operator()(const uint32_t x) const {
 		return hash_fn(x);
 	}
-	//typedef ska::power_of_two_hash_policy hash_policy;
 };
 
 uint32_t test_int(uint32_t n, uint32_t x0)
 {
 	uint32_t i, x, z = 0;
-	ska::bytell_hash_map<uint32_t, uint32_t, Hash32> h;
-	h.max_load_factor(0.75f);
+	boost::unordered_flat_map<uint32_t, uint32_t, Hash32> h;
 	for (i = 0, x = x0; i < n; ++i) {
 		x = hash32(x);
 #ifndef UDB2_TEST_DEL
